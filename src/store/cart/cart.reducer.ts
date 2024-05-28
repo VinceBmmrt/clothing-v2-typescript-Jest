52d533140c939fc4a0daea69aa3ca037bf311df6
VinceBmmrt <vincebommert75@gmail.com>
2024-05-28 17:03:49 +0200

    typing redux store + middleware

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -1,11 +1,11 @@
-import { AnyAction } from 'redux';
+import { AnyAction } from "redux";
 
-import { CartItem } from './cart.types';
-import { setIsCartOpen, setCartItems } from './cart.action';
+import { setCartItems, setIsCartOpen } from "./cart.action";
+import { CartItem } from "./cart.types";
 
 export type CartState = {
-  isCartOpen: boolean;
-  cartItems: CartItem[];
+  readonly isCartOpen: boolean;
+  readonly cartItems: CartItem[];
 };
 
 export const CART_INITIAL_STATE: CartState = {
