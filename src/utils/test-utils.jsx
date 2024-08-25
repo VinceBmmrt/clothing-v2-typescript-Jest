4c9d0c2baa1d63b8e29ad5e48f1b00b2640cd45f
VinceBmmrt <vincebommert75@gmail.com>
2024-08-25 17:51:36 +0200

    upgrading tests

--- src/utils/test-utils.jsx
+++ src/utils/test-utils.jsx
@@ -2,18 +2,17 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
-import { rootReducer } from "../store/root-reducer"; // Ensure this path is correct
+import { rootReducer } from "../store/root-reducer";
 
-// Function to render a component with Redux and Router providers
 export function renderWithProviders(
   ui,
   {
     preloadedState = {},
-    store = createStore(rootReducer, preloadedState), // Ensure store is correctly created
+    // Automatically create a store instance if no store was passed in
+    store = createStore(rootReducer, preloadedState),
     ...renderOptions
   } = {}
 ) {
-  // Wrapper component to provide Redux store and Router context
   function Wrapper({ children }) {
     return (
       <Provider store={store}>
@@ -22,6 +21,6 @@ export function renderWithProviders(
     );
   }
 
-  // Render the UI with the Wrapper and return the store and query functions
+  // Return an object with the store and all of RTL's query functions
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
